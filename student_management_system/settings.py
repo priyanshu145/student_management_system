3fd91155232a0efd6002c6dbbc244a3324390846
Priyanshu Kushwaha <priyanshukushwaha145@gmail.com>
2026-01-31 21:04:12 +0530

    settings.py file updated

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -12,23 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-
-
-if 'PYTHONANYWHERE_DOMAIN' not in os.environ:
-    from dotenv import load_dotenv
-    load_dotenv()
-    load_dotenv(BASE_DIR / '.env')
+from dotenv import load_dotenv
 
 import pymysql
 pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
